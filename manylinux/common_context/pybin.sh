2e0e018334b73b710a735e3717c92fb7c57edff6
René Fritze <rene.fritze@wwu.de>
2021-11-26 16:13:04 +0100

    [ml] fixup python 3.10 usage

--- manylinux/common_context/pybin.sh
+++ manylinux/common_context/pybin.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-PYVER=$(python3.6 -c 'pyversions={"3.9":"cp39-cp39","3.8":"cp38-cp38","3.7":"cp37-cp37m", "3.5":"cp35-cp35m", "3.6":"cp36-cp36m"}\
-    ;import os;print(pyversions[os.environ["PYTHON_VERSION"]])')
+PYVER=$(python3.9 -c 'pyversions={"3.7":"cp37-cp37m", "3.5":"cp35-cp35m", "3.6":"cp36-cp36m"}\
+    ;pyversions.update({f"3.{i}": f"cp3{i}-cp3{i}" for i in range(8,18)});import os;print(pyversions[os.environ["PYTHON_VERSION"]])')
 export PYVER
 PYTHON_ROOT_DIR=/opt/python/${PYVER}
 export PYTHON_ROOT_DIR
